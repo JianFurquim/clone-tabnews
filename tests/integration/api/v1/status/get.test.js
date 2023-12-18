168829ad729b7f9281b5c1c76d13e16b20be6f38
Jian Furquim <furquimjian@gmail.com>
2023-12-17 22:16:13 -0300

    Update: Return in `api/status` DateTime current

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -2,3 +2,12 @@ test("GET to /api/v1/status should return status 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
 });
+
+test("GET to /api/v1/status should return current DateTime", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(parseUpdatedAt).toEqual(responseBody.updated_at);
+});
