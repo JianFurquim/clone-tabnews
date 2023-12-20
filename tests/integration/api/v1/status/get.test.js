e3fec3ba6e0bb8cf9e1a57f8751dadf54db94faa
Jian Furquim <furquimjian@gmail.com>
2023-12-19 21:48:32 -0300

    Update: Test from database version in `api/status`

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,19 +1,16 @@
 test("GET to /api/v1/status should return status 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
-});
 
-test("GET to /api/v1/status should return current DateTime", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
+  // Tests about current DateTime //
   const responseBody = await response.json();
   expect(responseBody.updated_at).toBeDefined();
 
   const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(parseUpdatedAt).toEqual(responseBody.updated_at);
-});
 
-test("GET to /api/v1/status should return informations PostGres DB", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  const responseBody = await response.json();
+  // Tests about Postgres DB version
   expect(responseBody.dependencies.database.version).toBeDefined();
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+
 });
