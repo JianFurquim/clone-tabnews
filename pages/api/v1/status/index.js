156ee7464f9656f26cc7772e27e7adff4559c9ef
Jian Furquim <furquimjian@gmail.com>
2023-12-19 21:46:34 -0300

    New: Max Connections in `api/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,14 +2,21 @@ import database from "infra/database.js";
 
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
-  const databaseVersionResults = await database.query("SELECT version();");
-  const databaseVersionValue = databaseVersionResults.rows[0].version;
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
 
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
       database: {
         version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
       },
     },
   });
